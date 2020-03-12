0dbff4a6dbb60f2f890c16f8a34b2af2d8424064
Boris Karaoglanov <bkaraoglanov@astraflare.com>
2020-03-12 12:04:06 -0700

    Chnaged proejct_name to cluster_name

--- iam-instance-profile.tf
+++ iam-instance-profile.tf
@@ -1,4 +1,4 @@
 resource "aws_iam_instance_profile" "this" {
-  name = "${var.project_name}-ec2-profile-k8s-masters"
+  name = "${var.cluster_name}-ec2-profile-k8s-masters"
   role = aws_iam_role.this.*.id[0]
 }
--- iam-role-policy-k8s-masters.tf
+++ iam-role-policy-k8s-masters.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role_policy" "this" {
-  name = "${var.project_name}-ec2-profile-k8s-masters"
+  name = "${var.cluster_name}-ec2-profile-k8s-masters"
   role = aws_iam_role.this.*.id[0]
 
   policy = <<EOF
